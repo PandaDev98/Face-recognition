000c13132903cff77d70f82c8ec2c086ac52d741
PandaDev <juandiegotovaria@hotmail.com>
2023-02-25 20:20:58 -0500

    first commit

---
+++ face_recognition.py
@@ -0,0 +1,107 @@
+import cv2
+import os
+import numpy as np
+
+# Function to read images from a directory and convert to grayscale
+def read_images(directory,size=(200,200)):
+    X = []
+    y = []
+    label_dict = {}  # dictionary to map directory names to integer labels
+    label_counter = 0  # counter to assign unique integer label to each person
+    img_name = {}
+    for root, dirs, files in os.walk(directory):
+        for file in files:            
+            try:
+                if file.endswith("jpg") or file.endswith("png"):
+                    path = os.path.join(root, file)
+                    label = os.path.basename(path).split(" ")[0]
+                    if label not in label_dict:
+                        label_dict[label] = label_counter
+                        label_counter += 1
+                    img = cv2.imread(path, 0)
+                    img = cv2.resize(img, size)  # resize the image to the desired size
+                    X.append(np.asarray(img, dtype=np.uint8))
+                    y.append(label_dict[label])
+            except Exception as e:
+                print(f"Error processing file {file}: {e}")
+    return [X, y,label_dict]
+
+try:
+
+    known_person_color = (0, 255, 0)  # green for known people
+    unknown_person_color = (0, 0, 255)  # red for unknown people
+
+    # Train face recognizer
+    base_pictures, labels,label_dict = read_images("base_pictures", size=(200, 200))
+
+
+    face_recognizer = cv2.face.EigenFaceRecognizer_create()
+    face_recognizer.train(base_pictures, np.array(labels, dtype=np.int32))
+
+    # Initialize video capture from camera
+    cap = cv2.VideoCapture(0)
+
+    # Face detection
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+    while True:
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+
+        # Convert frame to grayscale for face detection
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # Detect faces in the frame
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+
+        # Iterate over detected faces
+        for (x, y, w, h) in faces:
+            # Crop face from the frame
+            face = gray[y:y+h, x:x+w]
+
+            # Resize face for recognition (should be same size as training images)
+            face = cv2.resize(face, (200, 200))
+
+            # Recognize face using face recognizer
+            label, confidence = face_recognizer.predict(face)
+
+             # Determine color for label and rectangle based on whether recognized person is in database
+            if label in label_dict.values():
+                text_color = known_person_color
+                rect_color = known_person_color
+                message = "It's Tovs!"
+            else:
+                text_color = unknown_person_color
+                rect_color = unknown_person_color
+                message = "Not Tovs!"
+
+
+             # Get the name from the label_dict dictionary
+            name = list(label_dict.keys())[list(label_dict.values()).index(label)]
+
+            # Draw label and confidence on the frame
+            text = "{} ({:.2f}%)".format(label, confidence, name)
+            cv2.putText(frame, text, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, text_color, 2)
+
+            # Draw rectangle around the face
+            cv2.rectangle(frame, (x, y), (x+w, y+h), rect_color, 2)
+
+            # Add message below the label
+            cv2.putText(frame, message, (x, y+h+30), cv2.FONT_HERSHEY_SIMPLEX, 0.9, text_color, 2)
+
+                       
+            # Draw rectangle around the face
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+        # Display the resulting frame
+        cv2.imshow('frame', frame)
+
+        # Exit if 'q' key is pressed
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # When everything done, release the capture and close window
+    cap.release()
+    cv2.destroyAllWindows()
+except Exception as e:
+    print(f"Error: {e}")
